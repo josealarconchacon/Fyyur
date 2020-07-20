2cb4643111a440575969c449d49fc78c998dccd3
Jose Carlos <josealarconchacon@gmail.com>
2020-07-20 14:17:25 -0400

    Implement Show and Artist models

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -20,7 +21,8 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://josealarconchacon@localhost:5432/fyyur'
 # TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
@@ -38,8 +40,12 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    show = db.relationship('Show', backref='Venue', lazy=True)
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -52,11 +58,21 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_venue= db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    show = db.relationship('Show', backref='Artist', lazy=True)
+
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
+class Show(db.Model):
+  _tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime())
+  '''create relationships between show and Artist, Venue'''
+  artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
