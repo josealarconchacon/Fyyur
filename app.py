e5c607fc2438c27f499cecd3826180d3275839b7
Jose Carlos <josealarconchacon@gmail.com>
2020-07-20 14:30:45 -0400

    update model Show

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ class Show(db.Model):
   _tablename__ = 'Show'
   id = db.Column(db.Integer, primary_key=True)
   start_time = db.Column(db.DateTime())
-  '''create relationships between show and Artist, Venue'''
   artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
   venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
 #----------------------------------------------------------------------------#
