c1899b03f59c8cdf97924ceef68c0e570230bc22
Jose Carlos <josealarconchacon@gmail.com>
2020-07-24 12:54:33 -0400

    add tables

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, Response, flash, redirect, ur
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -29,7 +30,7 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -40,14 +41,20 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String)
-    show = db.relationship('Show', backref='Venue', lazy=True)
+    # relationship with associative table
+
+    # genres = db.relationship('Genre', secondary=venue_table, backref=db.backref('venues'))
+    # website = db.Column(db.String(120))
+    # seeking_talent = db.Column(db.Boolean, default=False)
+    # seeking_description = db.Column(db.String(120))
+    # show = db.relationship('Show', backref='Venue', lazy=True)
+
+    # def __repr__(self):
+    #     return f'<Venue {self.id} {self.name}>'
 
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -58,19 +65,47 @@ class Artist(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    website = db.Column(db.String(120))
-    seeking_venue= db.Column(db.Boolean)
-    seeking_description = db.Column(db.String)
-    show = db.relationship('Show', backref='Artist', lazy=True)
+    # relationship with associative table
+
+    # genres = db.relationship('Genre', secondary=genre_table, backref=db.backref('artists'))
+    # website = db.Column(db.String(120))
+    # seeking_venue= db.Column(db.Boolean, default=False)
+    # seeking_description = db.Column(db.String(120))
+    # shows = db.relationship('Show', backref='artist', lazy=True)
+
+    # def __repr__(self):
+    #     return f'<Artist {self.id} {self.name}>'
 
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
-  _tablename__ = 'Show'
+  _tablename__ = 'show'
   id = db.Column(db.Integer, primary_key=True)
-  start_time = db.Column(db.DateTime())
-  artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  # artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  # venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+  # def __repr__(self):
+  #       return f'<Show {self.id} {self.start_time} artist_id={artist_id} venue_id={venue_id}>'
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+# genre_table = db.Table('genre_table',
+#     db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+#     db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+# )
+
+# venue_table = db.Table('venue_table',
+#     db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+#     db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+# )
+
+db.create_all()
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
